030db1946216a17b858535d63fe40e1cb4fee3a8
oubidar-Abderrahim <oubidar.abderrahim@gmail.com>
2019-04-07 01:26:43 +0100

    nested component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,16 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
+import { ConvertToSpacePipe } from "./shared/convert-to-space.pipe";
+import { starComponent } from "./shared/star.component";
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacePipe,
+    starComponent
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,16 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { b } from "@angular/core/src/render3";
 
 @Component({
   selector: "pm-products",
-  templateUrl: "./product-list.component.html"
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = "Product List";
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = "cart";
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProduct = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.products;
+  }
+
+  filteredProduct: IProduct[];
   products: IProduct[] = [
     {
       productId: 2,
@@ -34,7 +48,27 @@ export class ProductListComponent {
     }
   ];
 
+  constructor() {
+    this.filteredProduct = this.products;
+    this.listFilter = "";
+  }
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    // do something at init
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter(
+      (product: IProduct) =>
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = "Product List : " + message;
   }
 }

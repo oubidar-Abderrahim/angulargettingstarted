208786d5ffa11b8aa294ab425427e85a225630e8
oubidar-Abderrahim <oubidar.abderrahim@gmail.com>
2019-04-10 14:22:28 +0100

    guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { ProductDetailComponent } from "./products/product-detail.component";
 import { RouterModule } from "@angular/router";
 import { WelcomeComponent } from "./home/welcome.component";
+import { ProductDetailGuard } from "./products/product-detail.guard";
 
 @NgModule({
   declarations: [
@@ -25,10 +26,14 @@ import { WelcomeComponent } from "./home/welcome.component";
     HttpClientModule,
     RouterModule.forRoot([
       { path: "products", component: ProductListComponent },
-      { path: "products/:id", component: ProductDetailComponent },
-      { path: "welcome", component: WelcomeComponent }
-      { path: '', redirectTo: "welcome", pathMatch: 'full' }
-      { path: '**', redirectTo: "welcome", pathMatch: 'full' }
+      {
+        path: "products/:id",
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent
+      },
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", redirectTo: "welcome", pathMatch: "full" }
     ])
   ],
   bootstrap: [AppComponent]
--- src/app/products/product-detail.guard.ts
+++ src/app/products/product-detail.guard.ts
@@ -17,6 +17,12 @@ export class ProductDetailGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
+    let id = +next.url[1].path;
+    if (isNaN(id) || id < 1) {
+      alert("Invalid product Id");
+      this.router.navigate(["/products"]);
+      return false;
+    }
     return true;
   }
 }
